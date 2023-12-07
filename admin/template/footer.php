78188d71cd9f58f48b686343ae0b9a334de51329
LuizLacerdz <126706252+LuizLacerdz@users.noreply.github.com>
2023-12-07 14:15:45 -0300

    subir arquivos

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -4,8 +4,10 @@
         <script src = "../js/sidebars.js"></script>
         <script src = "../js/cadRelogio.js"></script>
         <script src = "../js/cadJoias"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js"></script>
         <script src = "/js/graficos.js"></script>
         <script src = "/js/graficosFuncionais.js"></script>
+        <script src = "/js/scripts.js"></script>
         
     </footer>
     </body>
--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
   <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
       <div class="position-sticky pt-3 sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
@@ -10,15 +10,15 @@
               </a>
             </li>
           <li class="mb-1">
-          <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-produto" aria-expanded="false">
-            Menu
+          <button class="link-light btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-produto" aria-expanded="false">
+          Menu
           </button>
           <div class="collapse" id="dashboard-produto">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-              <li><a href="../admin/cadastroRelogio.php" class="link-dark d-inline-flex text-decoration-none rounded"><h6>Cadastro de Relógios</h6></a></li>
-              <li><a href="../admin/listarRelogio.php" class="link-dark d-inline-flex text-decoration-none rounded"><h6>Listar Relógios</h6></a></li>
-              <li><a href="../admin/cadastroJoias.php" class="link-dark d-inline-flex text-decoration-none rounded"><h6>Cadastro de Joias</h6></a></li>
-              <li><a href="../admin/listarJoias.php" class="link-dark d-inline-flex text-decoration-none rounded"><h6>Listar Joias</h6></a></li>
+              <li><a href="../admin/cadastroRelogio.php" class="link-light d-inline-flex text-decoration-none rounded"><h6>Cadastro de Relógios</h6></a></li>
+              <li><a href="../admin/listarRelogio.php" class="link-light d-inline-flex text-decoration-none rounded"><h6>Listar Relógios</h6></a></li>
+              <li><a href="../admin/cadastroJoias.php" class="link-light d-inline-flex text-decoration-none rounded"><h6>Cadastro de Joias</h6></a></li>
+              <li><a href="../admin/listarJoias.php" class="link-light d-inline-flex text-decoration-none rounded"><h6>Listar Joias</h6></a></li>
             </ul>
           </div>
           </li>
